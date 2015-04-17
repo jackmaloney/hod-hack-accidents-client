56fdedf36ad9aa64ed26212612128360fe061b96
Jonathan Pagel <pagel@pobox.com>
2015-04-17 09:43:33 +0100

    wip

--- server.rb
+++ server.rb
@@ -1,14 +1,23 @@
 require 'sinatra'
+require 'json'
 
 before '/' do
   content_type :json
 end
 
 get '/' do
-  '{"status":"OK","data":"Thankyou for getting"}'
+  data = {
+    :status => "OK",
+    :data => "Thank you for getting at " + Time.now.to_s
+  }
+  data.to_json
 end
 
 post '/' do
-  '{"status":"OK","data":"Thankyou for posting"}'
+  data = {
+    :status => "OK",
+    :data => "Thank you for posting at " + Time.now.to_s
+  }
+  data.to_json
 end
 
